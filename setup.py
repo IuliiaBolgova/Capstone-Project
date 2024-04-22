8731de5da98ab7c51fea3d5d8da1285eca0fdb33
Iuliia Bolgova <163222078+IuliiaBolgova@users.noreply.github.com>
2024-04-22 11:17:59 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='(base) juliabolgova@Julias-MacBook-Pro ~ % cd',
+    author='Last login: Fri Apr 19 10:10:28 on ttys000',
+    license='MIT',
+)
